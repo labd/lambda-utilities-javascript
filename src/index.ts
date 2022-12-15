164e27c5940ee08cf4b6d2a13278172016aa3150
heston2 <104011261+heston2@users.noreply.github.com>
2022-12-15 10:21:29 +0000

    add opentelemetry helper functions (#5)
    * update packages, disable tracing sentry
    
    * feat: add otel helper functions
    
    * fix: lint

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import apollo from './apollo';
 import { Sentry } from './sentry';
 import { sendSqsMessage } from './sqs';
 import { getSecret } from './secrets';
+import opentelemetry from './opentelemetry';
 
-export { getSecret, Sentry, sendSqsMessage, koa, apollo };
+export { getSecret, Sentry, sendSqsMessage, koa, apollo, opentelemetry };
